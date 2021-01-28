9c37b615407d004512ec8be9ff9731df63ea4711
dimitri20 <dimitri.gulua@geolab.edu.ge>
2021-01-28 18:53:10 +0400

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randYear = rand.Next(2021);
+            if(randYear % 4 == 0)
+            {
+                Console.WriteLine($"{randYear} iyo nakiani weli");
+            } else
+            {
+                Console.WriteLine($"{randYear} ar iyo nakiani weli");
+            }
+        }
+    }
+}
---
+++ RandomArraySort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(200);
+            }
+
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+
+        }
+    }
+}
